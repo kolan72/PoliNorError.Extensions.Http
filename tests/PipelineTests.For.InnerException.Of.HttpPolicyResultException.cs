96d2eb93f6a8b3590abce5ec438eeb9f67f1b54e
kolan72 <kolan72@gmail.com>
2025-03-13 14:47:00 +0300

    Add tests for `HttpPolicyResultException.InnerException` when `HttpRequestException` is thrown outside the pipeline, with and without filter.

---
+++ tests/PipelineTests.For.InnerException.Of.HttpPolicyResultException.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+using System.Net.Http;
+
+namespace PoliNorError.Extensions.Http.Tests
+{
+	internal partial class PipelineTests
+	{
+		[Test]
+		[TestCase(true)]
+		[TestCase(false)]
+		public void Should_InnerException_Be_HttpRequestException_For_Out_Of_Pipeline_Source_Of_HttpRequestException_With_Many_PipelineHandlers(bool filterExists)
+		{
+			var i = 0;
+			var k = 0;
+
+			HttpErrorFilterCriteria criteria;
+			if (filterExists)
+			{
+				criteria = HttpErrorFilter.HandleHttpRequestException();
+			}
+			else
+			{
+				criteria = HttpErrorFilter.None();
+			}
+
+			var services = new ServiceCollection();
+
+			services.AddFakeHttpClient()
+			.WithResiliencePipeline((empyConfig) => empyConfig
+														.AddPolicyHandler(new RetryPolicy(3).WithErrorProcessorOf((_) => k++))
+														.AddPolicyHandler(new RetryPolicy(3).WithErrorProcessorOf((_) => i++))
+														.AsFinalHandler(criteria)
+														);
+
+			var serviceProvider = services.BuildServiceProvider();
+
+			using (var scope = serviceProvider.CreateScope())
+			{
+				var sut = scope.ServiceProvider.GetRequiredService<IHttpClientFactory>().CreateClient("my-httpclient");
+				var request = new HttpRequestMessage(HttpMethod.Get, "/any");
+
+				var exception = Assert.ThrowsAsync<HttpPolicyResultException>(async () => await sut.SendAsync(request));
+
+				Assert.That(exception?.HasFailedResponse == true, Is.False);
+
+				if (filterExists)
+				{
+					Assert.That(exception?.IsErrorExpected == true, Is.True);
+					Assert.That(i, Is.EqualTo(12));
+				}
+				else
+				{
+					Assert.That(exception?.IsErrorExpected == true, Is.False);
+					Assert.That(i, Is.EqualTo(0));
+				}
+
+				Assert.That(k, Is.EqualTo(3));
+
+				Assert.That(exception?.ThrownByFinalHandler == true, Is.False);
+				Assert.That(exception?.InnerException?.GetType(), Is.EqualTo(typeof(HttpRequestException)));
+			}
+		}
+	}
+}
