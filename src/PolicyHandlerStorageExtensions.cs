f7d1412de032563e8a5c095ada736f705e42e5ba
kolan72 <kolan72@gmail.com>
2025-03-27 18:02:02 +0300

    Added `AddRetryHandler` to the library's pipeline builders

---
+++ src/PolicyHandlerStorageExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace PoliNorError.Extensions.Http
+{
+	public static class PolicyHandlerStorageExtensions
+	{
+		public static TStorage AddRetryHandler<TStorage>(this IPolicyHandlerStorage<TStorage> storage, RetryPolicy retryPolicy) where TStorage : IPolicyHandlerStorage<TStorage>
+		{
+			return storage.AddPolicyHandler(retryPolicy);
+		}
+	}
+}
---
+++ tests/PipelineTests.For.AddRetryHandler.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+using System;
+using System.Net.Http;
+
+namespace PoliNorError.Extensions.Http.Tests
+{
+	internal partial class PipelineTests
+	{
+		[Test]
+		public void Should_Handle_Retries_Correctly_When_Single_AddRetryHandler_Is_Used()
+		{
+			var i = 0;
+
+			var criteria = HttpErrorFilter.HandleHttpRequestException();
+
+			var retryPolicy = new RetryPolicy(3).WithErrorProcessorOf((_) => i++);
+
+			IPipelineBuilder pipelineFactory(IEmptyPipelineBuilder empyConfig) =>
+																		empyConfig
+																		.AddRetryHandler(retryPolicy)
+																		.AsFinalHandler(criteria);
+
+			var invoker = new HttpClientWithPipelineInvoker();
+			invoker.InvokeHttpClientWithFailure<HttpPolicyResultException>(pipelineFactory);
+
+			Assert.That(i, Is.EqualTo(3));
+		}
+	}
+
+	internal class HttpClientWithPipelineInvoker
+	{
+		public void InvokeHttpClientWithFailure<TFailure>(Func<IEmptyPipelineBuilder, IPipelineBuilder> pipelineFactory) where TFailure : Exception
+		{
+			var services = new ServiceCollection();
+			services
+				.AddFakeHttpClient()
+				.WithResiliencePipeline(pipelineFactory);
+
+			using (var serviceProvider = services.BuildServiceProvider())
+			{
+				using (var scope = serviceProvider.CreateScope())
+				{
+					var sut = scope.ServiceProvider.GetRequiredService<IHttpClientFactory>().CreateClient("my-httpclient");
+					var request = new HttpRequestMessage(HttpMethod.Get, "/any");
+
+					var exception = Assert.ThrowsAsync<TFailure>(async () => await sut.SendAsync(request));
+
+					Assert.That(exception?.InnerException?.GetType(), Is.EqualTo(typeof(HttpRequestException)));
+				}
+			}
+		}
+	}
+}
