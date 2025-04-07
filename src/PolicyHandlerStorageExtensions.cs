28aeacf4752bca168c0ffeb52277b953c67596cb
kolan72 <kolan72@gmail.com>
2025-04-07 17:59:20 +0300

    Added a new overload to `AddRetryHandler` method that accepts a `Func<IServiceProvider, RetryPolicy>` factory delegate.

--- src/PolicyHandlerStorageExtensions.cs
+++ src/PolicyHandlerStorageExtensions.cs
@@ -1,10 +1,17 @@
-﻿namespace PoliNorError.Extensions.Http
+﻿using System;
+
+namespace PoliNorError.Extensions.Http
 {
 	public static class PolicyHandlerStorageExtensions
 	{
 		public static TStorage AddRetryHandler<TStorage>(this IPolicyHandlerStorage<TStorage> storage, RetryPolicy retryPolicy) where TStorage : IPolicyHandlerStorage<TStorage>
 		{
 			return storage.AddPolicyHandler(retryPolicy);
+		}
+
+		public static TStorage AddRetryHandler<TStorage>(this IPolicyHandlerStorage<TStorage> storage, Func<IServiceProvider, RetryPolicy> retryPolicyFactory) where TStorage : IPolicyHandlerStorage<TStorage>
+		{
+			return storage.AddPolicyHandler(retryPolicyFactory);
 		}
 	}
 }
--- tests/PipelineTests.For.AddRetryHandler.cs
+++ tests/PipelineTests.For.AddRetryHandler.cs
@@ -26,6 +26,28 @@ namespace PoliNorError.Extensions.Http.Tests
 			Assert.That(exception?.InnerException?.GetType(), Is.EqualTo(typeof(HttpRequestException)));
 			Assert.That(i, Is.EqualTo(3));
 		}
+
+		[Test]
+		public void Should_Handle_Retries_Correctly_When_Single_AddRetryHandler_FromFactory_Is_Used()
+		{
+			var i = 0;
+
+			var criteria = HttpErrorFilter.HandleHttpRequestException();
+
+			IPipelineBuilder pipelineFactory(IEmptyPipelineBuilder empyConfig) =>
+																		empyConfig
+																		.AddRetryHandler((sp) =>
+																			{
+																				var countProvider = sp.GetRequiredService<IRetryProvider>();
+																				return new RetryPolicy(countProvider.RetryCount).WithErrorProcessorOf((_) => i++);
+																			})
+																		.AsFinalHandler(criteria);
+
+			var invoker = new HttpClientWithPipelineInvoker();
+			var exception = invoker.InvokeHttpClientWithFailureWithRetryProvider<HttpPolicyResultException>(pipelineFactory);
+			Assert.That(exception?.InnerException?.GetType(), Is.EqualTo(typeof(HttpRequestException)));
+			Assert.That(i, Is.EqualTo(3));
+		}
 	}
 
 	internal class HttpClientWithPipelineInvoker
@@ -46,5 +68,34 @@ namespace PoliNorError.Extensions.Http.Tests
 				return Assert.ThrowsAsync<TFailure>(async () => await sut.SendAsync(request));
 			}
 		}
+
+		public TFailure InvokeHttpClientWithFailureWithRetryProvider<TFailure>(Func<IEmptyPipelineBuilder, IPipelineBuilder> pipelineFactory) where TFailure : Exception
+		{
+			var services = new ServiceCollection();
+			services.AddScoped<IRetryProvider, RetryProvider>();
+
+			services
+				.AddFakeHttpClient()
+				.WithResiliencePipeline(pipelineFactory);
+
+			using (var serviceProvider = services.BuildServiceProvider())
+			using (var scope = serviceProvider.CreateScope())
+			{
+				var sut = scope.ServiceProvider.GetRequiredService<IHttpClientFactory>().CreateClient("my-httpclient");
+				var request = new HttpRequestMessage(HttpMethod.Get, "/any");
+
+				return Assert.ThrowsAsync<TFailure>(async () => await sut.SendAsync(request));
+			}
+		}
+	}
+
+	internal interface IRetryProvider
+	{
+		int RetryCount { get; }
+	}
+
+	internal class RetryProvider : IRetryProvider
+	{
+		public int RetryCount => 3;
 	}
 }
