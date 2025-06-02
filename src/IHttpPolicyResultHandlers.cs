95a7ec7bb4e4e24409f19d20f52912dc5c80a9f2
kolan72 <kolan72@gmail.com>
2025-06-02 12:25:26 +0300

    Introduced the `IHttpPolicyResultHandlers` interface.

---
+++ src/IHttpPolicyResultHandlers.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PoliNorError.Extensions.Http
+{
+	/// <summary>
+	/// Defines a fluent interface for registering synchronous and asynchronous handlers
+	/// to process the result of an HTTP policy execution.
+	/// </summary>
+	public interface IHttpPolicyResultHandlers
+	{
+		/// <summary>
+		/// Adds a synchronous policy result handler.
+		/// </summary>
+		/// <param name="syncHandler">A delegate that processes the <see cref="PolicyResult{HttpResponseMessage}" /> synchronously.</param>
+		/// <returns>The current <see cref="IHttpPolicyResultHandlers"/> instance to enable fluent chaining.</returns>
+		/// <returns>
+		/// The <see cref="IHttpPolicyResultHandlers"/> instance for method chaining.
+		/// </returns>
+		IHttpPolicyResultHandlers AddHandler(Action<PolicyResult<HttpResponseMessage>, CancellationToken> syncHandler);
+
+		/// <summary>
+		/// Adds an asynchronous policy result handler.
+		/// </summary>
+		/// <param name="asyncHandler">A delegate that processes the <see cref="PolicyResult{HttpResponseMessage}" /> asynchronously.</param>
+		/// <returns>
+		/// The <see cref="IHttpPolicyResultHandlers"/> instance for method chaining.
+		/// </returns>
+		IHttpPolicyResultHandlers AddHandler(Func<PolicyResult<HttpResponseMessage>, CancellationToken, Task> asyncHandler);
+	}
+
+	internal class HttpPolicyResultHandlers : IHttpPolicyResultHandlers
+	{
+		private readonly List<HttpPolicyResultHandler> _hanlders = new List<HttpPolicyResultHandler>();
+
+		public IHttpPolicyResultHandlers AddHandler(Action<PolicyResult<HttpResponseMessage>, CancellationToken> syncHandler)
+		{
+			if (syncHandler == null)
+				throw new ArgumentNullException(nameof(syncHandler));
+
+			var handler = new HttpPolicyResultHandler(syncHandler);
+			_hanlders.Add(handler);
+			return this;
+		}
+
+		public IHttpPolicyResultHandlers AddHandler(Func<PolicyResult<HttpResponseMessage>, CancellationToken, Task> asyncHandler)
+		{
+			if (asyncHandler == null)
+				throw new ArgumentNullException(nameof(asyncHandler));
+
+			var handler = new HttpPolicyResultHandler(asyncHandler);
+			_hanlders.Add(handler);
+			return this;
+		}
+
+		internal void AttachTo(RetryPolicy retryPolicy)
+		{
+			foreach (var handler in _hanlders)
+			{
+				handler.AttachTo(retryPolicy);
+			}
+		}
+	}
+}
---
+++ tests/HttpPolicyResultHandlersTests.cs
@@ -0,0 +1,87 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PoliNorError.Extensions.Http.Tests
+{
+	internal class HttpPolicyResultHandlersTests
+	{
+		[Test]
+		public void Should_Attach_SyncHandler_To_RetryPolicy()
+		{
+			bool invoked = false;
+			var handlers = new HttpPolicyResultHandlers();
+			var policy = new RetryPolicy(1);
+
+			handlers.AddHandler((_, __) => invoked = true);
+			handlers.AttachTo(policy);
+
+			using (var failedHttpResponse = new HttpResponseMessage())
+			{
+				policy.Handle(() => failedHttpResponse);
+				Assert.That(invoked, Is.True);
+			}
+		}
+
+		[Test]
+		public void Should_Attach_AsyncHandler_To_RetryPolicy_For_SyncHandling()
+		{
+			bool invoked = false;
+			var handlers = new HttpPolicyResultHandlers();
+			var policy = new RetryPolicy(1);
+
+			handlers.AddHandler((_, __) => { invoked = true; return Task.CompletedTask; });
+			handlers.AttachTo(policy);
+
+			using (var failedHttpResponse = new HttpResponseMessage())
+			{
+				policy.Handle(() => failedHttpResponse);
+				Assert.That(invoked, Is.True);
+			}
+		}
+
+		[Test]
+		public async Task Should_Attach_AsyncHandler_To_RetryPolicy_For_AsyncHandling()
+		{
+			bool invoked = false;
+			var handlers = new HttpPolicyResultHandlers();
+			var policy = new RetryPolicy(1);
+
+			handlers.AddHandler(async (_, __) => { invoked = true; await Task.Delay(1); });
+			handlers.AttachTo(policy);
+
+			using (var failedHttpResponse = new HttpResponseMessage())
+			{
+				await policy.HandleAsync(async(__) => { await Task.Delay(1); return failedHttpResponse; });
+				Assert.That(invoked, Is.True);
+			}
+		}
+
+		[Test]
+		public void Should_ThrowArgumentNullException_When_AddingNullSyncHandler()
+		{
+			// Arrange
+			var handlers = new HttpPolicyResultHandlers();
+
+			// Act & Assert
+			Assert.That(() => handlers.AddHandler((Action<PolicyResult<HttpResponseMessage>, CancellationToken>)null),
+				Throws.ArgumentNullException.With.Property("ParamName").EqualTo("syncHandler"));
+		}
+
+		[Test]
+		public void Should_ThrowArgumentNullException_When_AddingNullAsyncHandler()
+		{
+			// Arrange
+			var handlers = new HttpPolicyResultHandlers();
+
+			// Act & Assert
+			Assert.That(() => handlers.AddHandler(null),
+				Throws.ArgumentNullException.With.Property("ParamName").EqualTo("asyncHandler"));
+		}
+	}
+}
