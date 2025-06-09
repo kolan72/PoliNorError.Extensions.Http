12b4ba5fb4d493dc75468ca7c2a93aa174f276c9
kolan72 <kolan72@gmail.com>
2025-06-09 18:48:59 +0300

    Introduce the `RetryPolicyOptions` class.

---
+++ tests/RetryPolicyOptionsTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace PoliNorError.Extensions.Http.Tests
+{
+	internal class RetryPolicyOptionsTests
+	{
+		[Test]
+		public void Should_Set_And_Get_RetryDelay()
+		{
+			// Arrange
+			var options = new RetryPolicyOptions();
+			var expectedDelay = ConstantRetryDelay.Create(TimeSpan.FromTicks(1));
+
+			// Act
+			options.RetryDelay = expectedDelay;
+
+			// Assert
+			Assert.That(options.RetryDelay, Is.EqualTo(expectedDelay));
+		}
+	}
+}
