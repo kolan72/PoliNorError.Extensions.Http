90dd9797a7932ec7489ed33ef6331670e6a1a672
kolan72 <kolan72@gmail.com>
2025-06-12 19:17:57 +0300

    Add docs to `AddRetryHandler<TStorage>` methods.

--- src/PolicyHandlerStorageExtensions.Retry.FromOptions.cs
+++ src/PolicyHandlerStorageExtensions.Retry.FromOptions.cs
@@ -4,11 +4,29 @@ namespace PoliNorError.Extensions.Http
 {
 	public static partial class PolicyHandlerStorageExtensions
 	{
+		/// <summary>
+		/// Adds a handler based on a RetryPolicy to a pipeline builder
+		/// that implements the <see cref="IPolicyHandlerStorage{TStorage}"/> interface.
+		/// </summary>
+		/// <typeparam name="TStorage">Storage type for <see cref="System.Net.Http.DelegatingHandler"/>.</typeparam>
+		/// <param name="storage">Storage for <see cref="System.Net.Http.DelegatingHandler"/>.</param>
+		/// <param name="retryCount">Number of retries.</param>
+		/// <param name="options"><see cref="RetryPolicyOptions"/>.</param>
+		/// <returns></returns>
 		public static TStorage AddRetryHandler<TStorage>(this IPolicyHandlerStorage<TStorage> storage, int retryCount, RetryPolicyOptions options) where TStorage : IPolicyHandlerStorage<TStorage>
 		{
 			return storage.AddRetryHandler(RetryCountInfo.Limited(retryCount), options);
 		}
 
+		/// <summary>
+		/// Adds a handler based on a RetryPolicy to a pipeline builder
+		/// that implements the <see cref="IPolicyHandlerStorage{TStorage}"/> interface.
+		/// </summary>
+		/// <typeparam name="TStorage">Storage type for <see cref="System.Net.Http.DelegatingHandler"/>.</typeparam>
+		/// <param name="storage">Storage for <see cref="System.Net.Http.DelegatingHandler"/>.</param>
+		/// <param name="retryCount">Number of retries.</param>
+		/// <param name="configure">Delegate for configuring <see cref="RetryPolicyOptions"/>.</param>
+		/// <returns></returns>
 		public static TStorage AddRetryHandler<TStorage>(this IPolicyHandlerStorage<TStorage> storage, int retryCount, Action<RetryPolicyOptions> configure = null) where TStorage : IPolicyHandlerStorage<TStorage>
 		{
 			var options = new RetryPolicyOptions();
