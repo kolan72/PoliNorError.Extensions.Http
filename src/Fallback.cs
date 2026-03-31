5bf5dfe0dbef48315bdf13faf411c3fdd2076827
kolan72 <kolan72@gmail.com>
2026-03-31 14:32:59 +0300

    Add `Fallback` class for `FallbackOptions`.

---
+++ src/Fallback.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PoliNorError.Extensions.Http
+{
+	public class Fallback
+	{
+		public Fallback(Func<CancellationToken, HttpResponseMessage> func)
+		{
+			Func = func ?? throw new ArgumentNullException(nameof(func));
+		}
+
+		internal Func<CancellationToken, HttpResponseMessage> Func { get; set; }
+
+		public static implicit operator Func<CancellationToken, Task<HttpResponseMessage>>(Fallback fallback)
+			=> ct => Task.FromResult(fallback.Func(ct));
+	}
+}
