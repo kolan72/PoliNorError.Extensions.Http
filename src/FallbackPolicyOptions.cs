83316dd48b5d247e0416ee48d9ac4662c19ed824
kolan72 <kolan72@gmail.com>
2026-04-08 13:26:54 +0300

    Introduced the `FallbackPolicyOptions` class.

---
+++ src/FallbackPolicyOptions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PoliNorError.Extensions.Http
+{
+	/// <summary>
+	/// Represents configuration options for a fallback-based policy. Inherits common
+	/// policy configuration settings from <see cref="PolicyOptions"/> and provides
+	/// additional settings to control fallback behavior.
+	/// </summary>
+	public class FallbackPolicyOptions : PolicyOptions
+	{
+		public Func<CancellationToken, Task<HttpResponseMessage>> Fallback { get; set; }
+	}
+}
