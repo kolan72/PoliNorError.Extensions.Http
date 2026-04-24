8bb344f8566253cf50d2f0be19350f6d1593408a
kolan72 <kolan72@gmail.com>
2026-04-24 18:56:02 +0300

    Added an `AddFallbackHandler` overload to pipeline builders that accepts a `Func<CancellationToken, Task<HttpResponseMessage>>` and `FallbackPolicyOptions`.

---
+++ src/PolicyHandlerStorageExtensions.Fallback.FromOptions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PoliNorError.Extensions.Http
+{
+	public static partial class PolicyHandlerStorageExtensions
+	{
+		internal static TStorage AddFallbackHandler<TStorage>(
+			this IPolicyHandlerStorage<TStorage> storage,
+			Func<CancellationToken, Task<HttpResponseMessage>> func,
+			FallbackPolicyOptions options) where TStorage : IPolicyHandlerStorage<TStorage>
+		{
+			if (options is null)
+				throw new ArgumentNullException(nameof(options));
+
+			var bep = new BulkErrorProcessor();
+			if (!(options.ConfigureErrorProcessing is null))
+			{
+				options.ConfigureErrorProcessing(bep);
+			}
+
+			var res = new FallbackPolicy(bep).WithAsyncFallbackFunc(func);
+
+			if (!(options.ConfigurePolicyResultHandling is null))
+			{
+				var handlers = new HttpPolicyResultHandlers();
+				options.ConfigurePolicyResultHandling(handlers);
+				handlers.AttachTo(res);
+			}
+
+			if (!(options.ConfigureErrorFilter is null))
+			{
+				res.AddErrorFilter(options.ConfigureErrorFilter);
+			}
+
+			if (!(options.PolicyName is null))
+			{
+				res.WithPolicyName(options.PolicyName);
+			}
+			return storage.AddPolicyHandler(res);
+		}
+	}
+}
